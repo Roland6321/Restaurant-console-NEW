654413107032e72a06eae9b2c92ab914de655dad
roland6321 <radocz.roland@gmail.com>
2024-04-22 13:16:32 +0200

    restoring

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+document.getElementById('submitBtn').addEventListener('click', function() {
+    var password = document.getElementById('passwordInput').value;
+    if (password === '1234') {
+        document.getElementById('welcome').classList.add('hidden');
+        document.getElementById('activeOrders').classList.remove('hidden');
+    } else {
+        document.getElementById('alert').classList.remove('hidden');
+    }
+});
+
+document.getElementById('historyBtn').addEventListener('click', function() {
+    document.getElementById('activeOrders').classList.add('hidden');
+    document.getElementById('orderHistory').classList.remove('hidden');
+});
+
+document.getElementById('ordersBtnHistory').addEventListener('click', function() {
+    document.getElementById('orderHistory').classList.add('hidden');
+    document.getElementById('activeOrders').classList.remove('hidden');
+});
+
+
+
 
