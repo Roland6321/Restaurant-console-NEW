b089cdf41352a38f8ae1d46cb23713d3db93dccd
roland6321 <radocz.roland@gmail.com>
2024-04-23 11:00:13 +0200

    new version

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
+function showSection(sectionId) {
+    // Hide all sections
+    document.querySelectorAll('.page').forEach(page => {
+        page.style.display = 'none';
+    });
+
+    // Show the requested section
+    document.getElementById(sectionId).style.display = 'block';
+}
+
 document.getElementById('submitBtn').addEventListener('click', function() {
     var password = document.getElementById('passwordInput').value;
-    if (password === '1234') { // Assuming '1234' is your test password
-        document.getElementById('welcome').classList.add('hidden');
-        document.getElementById('activeOrders').classList.remove('hidden');
+    // Ensure the alert is hidden initially on each submit
+    document.getElementById('alert').style.display = 'none';
+
+    if (password === '1234') { // Correct password
+        showSection('activeOrders');
     } else {
-        document.getElementById('alert').classList.remove('hidden');
+        // Only display the alert if the password is incorrect
+        document.getElementById('alert').style.display = 'block';
     }
 });
 
 document.getElementById('historyBtn').addEventListener('click', function() {
-    document.getElementById('activeOrders').classList.add('hidden');
-    document.getElementById('orderHistory').classList.remove('hidden');
+    showSection('orderHistory');
 });
 
 document.getElementById('ordersBtnHistory').addEventListener('click', function() {
-    document.getElementById('orderHistory').classList.add('hidden');
-    document.getElementById('activeOrders').classList.remove('hidden');
+    showSection('activeOrders');
 });
+
+
+
+
 
 
 
